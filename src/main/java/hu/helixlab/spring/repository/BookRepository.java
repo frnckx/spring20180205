9eff29465b415dc589ab1291fc9fdccf5a55a065
bocz.tivadar@lanoga.hu <bocz.tivadar@lanoga.hu>
2018-02-14 20:12:17 +0100

    init

--- src/main/java/hu/helixlab/spring/repository/BookRepository.java
+++ src/main/java/hu/helixlab/spring/repository/BookRepository.java
@@ -1,18 +1,14 @@
 package hu.helixlab.spring.repository;
 
 import hu.helixlab.spring.domain.Book;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface BookRepository extends CrudRepository<Book, Integer>{
 
 
-	@Query(value = "SELECT b From Book where b.id > :id")
-	List<Book> findAllById(@Param("id") String id);
+//	@Query(value = "SELECT b From Book where b.id > :id")
+//	List<Book> findAllById(@Param("id") String id);
 
 }
